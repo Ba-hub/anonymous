9f1fec8bb0746b266981544825deea57d66c8343
Ba-hub <54659606+Ba-hub@users.noreply.github.com>
2020-03-07 13:11:06 +0530

    Create anonymous.py

---
+++ anonymous.py
@@ -0,0 +1,33 @@
+# coded by ghosthub
+import requests
+
+print("\n@N0NYM0US E-m@il\n")
+print("-- coded by Gh0sthub")
+print("-- iconicbabay.simdif.com\n")
+to = raw_input('to: ')
+subject = raw_input('subject: ')
+message = raw_input('message: ')
+
+user_agent = 'Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
+sess = requests.Session()
+email_req = sess.post('http://anonymouse.org/cgi-bin/anon-email.cgi', headers={
+	'Host': 'anonymouse.org',
+	'User-Agent': user_agent,
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Language': 'en-US,en;q=0.5',
+	'Accept-Encoding': 'gzip, deflate',
+	'Referer': 'http://anonymouse.org/anonemail.html',
+        'Connection': 'close',
+        'Upgrade-Insecure-Requests':'1',
+        'Content-Type':'application/x-www-form-urlencoded'
+}, data={
+	'to': to,
+	'subject': subject,
+	'text': message
+})
+
+if 'The e-mail has been sent' in email_req.text:
+    print("[+] Email Sent!")
+    print("[+] In order to increase your privacy, the anonymous e-mail will be randomly delayed up to 12 hours")
+
+
